47c5e02420cd874170b67e692147db0a931c6c0c
Groosha <groosha@protonmail.com>
2023-09-29 13:11:35 +0800

    Use old-style Optional for python 3.9 compatibility

--- python/config_reader.py
+++ python/config_reader.py
@@ -1,3 +1,5 @@
+from typing import Optional
+
 from pydantic import SecretStr
 from pydantic_settings import BaseSettings
 
@@ -6,7 +8,7 @@ class Config(BaseSettings):
     bot_token: SecretStr
     mode: str = "polling"  # "polling" or "webhook"
     drop_updates_on_restart: bool = False
-    webhook_addr: str | None = None
+    webhook_addr: Optional[str] = None
     webhook_path: str = "/"
     webhook_secret: SecretStr = "aaaaa"
 
