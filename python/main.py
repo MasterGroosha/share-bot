25f6bc15511c3a477cef67ae08877671805a201c
Groosha <groosha@protonmail.com>
2023-09-29 13:01:36 +0800

    Webhook mode support (mostly for gunicorn [deta.space / ngrok])

--- python/main.py
+++ python/main.py
@@ -1,6 +1,8 @@
 import asyncio
 
 from aiogram import Bot, Dispatcher
+from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
+from aiohttp import web
 
 from config_reader import Config, get_config
 from handlers import router
@@ -16,9 +18,24 @@ async def main():
     dispatcher = Dispatcher()
     dispatcher.include_router(router)
 
+    await bot.delete_webhook(drop_pending_updates=config.drop_updates_on_restart)
+
     if config.mode == "polling":
-        await bot.delete_webhook(drop_pending_updates=config.drop_updates_on_restart)
         await dispatcher.start_polling(bot)
+    else:
+        app = web.Application()
+        webhook_handler = SimpleRequestHandler(
+            dispatcher=dispatcher,
+            bot=bot,
+            secret_token=config.webhook_secret.get_secret_value()
+        )
+        webhook_handler.register(app, path=config.webhook_path)
+        setup_application(app, dispatcher, bot=bot)
+        await bot.set_webhook(
+            url=config.webhook_addr + config.webhook_path,
+            secret_token=config.webhook_secret.get_secret_value()
+        )
+        return app
 
 
 if __name__ == '__main__':
